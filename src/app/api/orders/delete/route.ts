1e4f169f898d8bd9f251ba3178e7bd10f94c5fa3
Pedram <p.vosooghinia69@gmail.com>
2025-02-04 22:40:10 +0330

    fix build

--- src/app/api/orders/delete/route.ts
+++ src/app/api/orders/delete/route.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import db from "@/db";
+import { NextResponse, NextRequest } from "next/server";
+import { query } from "@/db";
 
-export async function DELETE(req) {
+export async function DELETE(req:NextRequest) {
   try {
     const { orderId } = await req.json();
-    const query = `
+    const sqlQuery = `
       DELETE FROM pre_forma
       WHERE id = $1
       RETURNING *;
     `;
 
-    const result = await db.query(query, [orderId]);
+    const result = await query(sqlQuery, [orderId]);
 
-    if (result.rowCount > 0) {
+    if (result.rows.length > 0) {
       return NextResponse.json({ success: true, data: result.rows[0] });
     } else {
       return NextResponse.json({
